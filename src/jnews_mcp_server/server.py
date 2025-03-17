e730cb97b0762f7a6a766c9dbcc2be4cc269487f
juhemcp <dev@juheapi.com>
2025-03-17 17:29:19 +0800

    update

--- src/jnews_mcp_server/server.py
+++ src/jnews_mcp_server/server.py
@@ -145,7 +145,7 @@ async def main():
             write_stream,
             InitializationOptions(
                 server_name="jnews-mcp-server",
-                server_version="0.1.0",
+                server_version="0.1.2",
                 capabilities=server.get_capabilities(
                     notification_options=NotificationOptions(),
                     experimental_capabilities={},
